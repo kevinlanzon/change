2356aed4975c65c67be9f6bf15b00d8b5a3cd7e2
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-28 18:21:41 +0100

    refactors tests

--- spec/coin_change_spec.rb
+++ spec/coin_change_spec.rb
@@ -2,39 +2,16 @@ require 'coin_changer'
 
 describe CoinChanger do
 
-  it 'should give change for 1p' do
-    expect(subject.give_change(1)).to eq [1]
-  end
-
-  it 'should give change for 2p' do
-    expect(subject.give_change(2)).to eq [1,1]
-  end
-
-  it 'should give change for 3p' do
-    expect(subject.give_change(3)).to eq [1,1,1]
-  end
-
-  it 'should give change for 4p' do
-    expect(subject.give_change(4)).to eq [1,1,1,1]
-  end
-
-  it 'should give change for 5p' do
-    expect(subject.give_change(5)).to eq [5]
-  end
-
-  it 'should give change for 6p' do
-    expect(subject.give_change(6)).to eq [5,1]
-  end
-
-  it 'should give change for 7p' do
-    expect(subject.give_change(7)).to eq [5,1,1]
-  end
-
-  it 'should give change for 8p' do
-    expect(subject.give_change(8)).to eq [5,1,1,1]
-  end
-
-  it 'should give change for 9p' do
-    expect(subject.give_change(9)).to eq [5,1,1,1,1]
+  [[1,[1]],
+   [2,[1,1]],
+   [3,[1,1,1]],
+   [4,[1,1,1,1]],
+   [5,[5]],
+   [6,[5,1]],
+   [9,[5,1,1,1,1]],
+  ].each do |amount, coins|
+    it "should give change for #{amount}" do
+      expect(subject.give_change(amount)).to eq coins
+    end
   end
 end
