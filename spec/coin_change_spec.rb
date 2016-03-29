2fb076897d79d7ce95cf7b993f257c34774a2e75
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-29 10:51:54 +0100

    gives change for 6p

--- spec/coin_change_spec.rb
+++ spec/coin_change_spec.rb
@@ -21,4 +21,8 @@ describe CoinChanger do
   it 'gives change for 5p' do
     expect(subject.give_change(5)).to eq [5]
   end
+
+  it 'gives change for 6p' do
+    expect(subject.give_change(6)).to eq [5,1]
+  end
 end
